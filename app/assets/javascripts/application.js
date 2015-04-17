fd5737162be315d493f90920b108bbcb44003a3d
mbouzi <mrbouzi@gmail.com>
2015-04-17 17:56:27 -0400

    menu design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,4 +25,14 @@ $(document).ready(function() {
         $(".signup").toggle();
     });
 
+});
+
+$(document).ready(function() {
+
+    $("ul").hover(function(){
+        $(".line").css("margin-top", ".8em");
+    }, function() {
+    	$(".line").css("margin-top", ".4em");
     });
+});
+
