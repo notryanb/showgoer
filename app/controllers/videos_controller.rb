81e542cd19b034b62514210cbcf487ade4195dcc
notryanb <notryanb@gmail.com>
2015-04-17 18:55:34 -0400

    Fix Video Post issues

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,18 +6,14 @@ class VideosController < ApplicationController
 
   def new
      @s3_direct_post = S3_BUCKET.presigned_post(key: "#{SecureRandom.uuid}"+"${filename}", success_action_status: 201, acl: :public_read)
-
-     p "PRESEIGNED POST URL:        #{@s3_direct_post.url}"
   end
 
   def create
 
-
-    p "I GOT TO THE CREATE ROUTE VIA AJAX #{params[:url]}"
     @video = Video.new(url: params[:url])
 
     if @video.save
-      redirect_to videos_path, success: 'File successfully uploaded'
+      redirect_to '/videos'
     else
       flash.now[:notice] = 'There was an error'
       render :new
