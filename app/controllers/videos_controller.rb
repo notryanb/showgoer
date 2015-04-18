b2135902dfe62884485c85560a345d3e7a62918c
notryanb <notryanb@gmail.com>
2015-04-18 15:09:30 -0400

    Concert show page testing. Should be moved to concert controller from video controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,16 +1,20 @@
 class VideosController < ApplicationController
 
   def index
-    @videos = Video.all
+    @concert = Concert.find_by_id(1501) 
+    @video_play_test = @concert.videos.last
+    @related_videos = @concert.videos
+
   end
 
   def new
      @s3_direct_post = S3_BUCKET.presigned_post(key: "#{SecureRandom.uuid}"+"${filename}", success_action_status: 201, acl: :public_read)
+
+     @test_concert_id = Concert.find_by_id(1501).id
   end
 
   def create
-
-    @video = Video.new(url: params[:url])
+    @video = Video.new(url: params[:url], concert_id: params[:concert_id])
 
     if @video.save
       redirect_to '/videos'
@@ -18,7 +22,11 @@ class VideosController < ApplicationController
       flash.now[:notice] = 'There was an error'
       render :new
     end
-
   end
+
+  def show
+    @video_play_test = Video.last
+  end
+
 
 end
