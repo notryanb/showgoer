60cdfb2df6208404e76a8e5f0a3cc51f8bc3c7e7
Phanendar Chitraju <phanendar@gmail.com>
2015-04-18 16:28:05 -0400

    added files that might cause merge conflicts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,12 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-$(document).ready(function() {
 
-    $("ul").hover(function(){
-        $(".line").css("margin-top", ".8em");
-    }, function() {
-    	$(".line").css("margin-top", ".4em");
-    });
-});
 
