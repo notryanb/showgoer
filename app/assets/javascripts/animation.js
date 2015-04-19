0cc7829276a634795c51823bb1b4e39793a77ba8
mbouzi <mrbouzi@gmail.com>
2015-04-19 15:29:53 -0400

    disappearance/reappearance of items

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -26,11 +26,25 @@ $(document).ready(function() {
 
     $("#menu").click(function(){
         $("#user_show").css("margin-left", "10em");
-        $(this).css("visibility", "hidden")
+        $(this).css("visibility", "hidden");
+        $("#logo").css("visibility", "hidden");
+        $("#signup_but").css("visibility", "hidden");
+        $("#login_but").css("visibility", "hidden");
+        $("#search_but").css("visibility", "hidden");
+        $("#title").css("visibility", "hidden");
+        $("#goer").css("visibility", "hidden");
+        $("#search").css("visibility", "hidden");
     });
 
     $("#ex3").click(function(){
         $("#user_show").css("margin-left", "82em");
-        $("#menu").css("visibility", "visible")
+        $("#menu").css("visibility", "visible");
+        $("#logo").css("visibility", "visible");
+        $("#signup_but").css("visibility", "visible");
+        $("#login_but").css("visibility", "visible");
+        $("#search_but").css("visibility", "visible");
+        $("#title").css("visibility", "visible");
+        $("#goer").css("visibility", "visible");
+        $("#search").css("visibility", "visible");
     });
 });
