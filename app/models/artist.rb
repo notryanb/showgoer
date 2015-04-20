c0d9e7dc9e8dbb5f1b838538ca265ed83647699b
Phanendar Chitraju <phanendar@gmail.com>
2015-04-20 10:49:27 -0400

    refactored code from applications controller

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,31 @@ class Artist < ActiveRecord::Base
   has_many :appearances
   has_many :concerts, through: :appearances
   has_many :videos
+
+  def self.the_artist(artist)
+    return first_artist = artist.first
+  end
+
+  def self.past_concerts(artist_name)
+    pastconcerts = []
+    artist_concerts = Concert.concerts_for_artist_name(artist_name)
+    artist_concerts.each do |concert|
+      if DateTime.now > concert.date_time
+        pastconcerts << concert
+      end
+    end
+    return pastconcerts
+  end
+
+  def self.future_concerts(artist_name)
+    futureconcerts = []
+    artist_concerts = Concert.concerts_for_artist_name(artist_name)
+    artist_concerts.each do |concert|
+      if DateTime.now < concert.date_time
+        futureconcerts << concert
+      end
+    end
+    return futureconcerts
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,22 @@ class User < ActiveRecord::Base
   has_many :dislikes
   has_many :videos
 
+  def self.user_videos(userid)
+    @user_videos = Video.where(user_id: userid)
+  end
+
+  def self.user_contributed_concerts(userid)
+    concert_ids = []
+    user_concerts = []
+    user_videos = Video.where(user_id: userid)
+    user_videos.each do |video|
+      concert_ids << video.concert_id
+    end
+
+    concert_ids.each do |concertid|
+      user_concerts << Concert.find_by(id: concertid)
+    end
+    return user_concerts
+  end
+
 end
