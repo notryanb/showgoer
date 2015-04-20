9255e4e3854c1c5973f64aac527548a9ae6091f8
notryanb <notryanb@gmail.com>
2015-04-20 08:36:37 -0400

    Like button Hover-over

--- app/controllers/dislikes_controller.rb
+++ app/controllers/dislikes_controller.rb
@@ -1,8 +1,6 @@
 class DislikesController < ApplicationController
 
   def create
-    p "PARAMS #{params}------------------"
-
     @dislike = Dislike.create(user_id: params[:user_id], video_id: params[:video_id])
 
     redirect_to :back
