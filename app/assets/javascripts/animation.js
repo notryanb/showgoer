84b80de080fdd1f74e8847f2d8dbd33efeddad84
mbouzi <mrbouzi@gmail.com>
2015-04-20 10:08:50 -0400

    fixed user show overflow

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -26,26 +26,10 @@ $(document).ready(function() {
 
     $("#menu").click(function(){
         $("#user_show").css("left", "10em");
-        $(this).css("visibility", "hidden");
-        $("#logo").css("visibility", "hidden");
-        $("#signup_but").css("visibility", "hidden");
-        $("#login_but").css("visibility", "hidden");
-        $("#search_but").css("visibility", "hidden");
-        $("#title").css("visibility", "hidden");
-        $("#goer").css("visibility", "hidden");
-        $("#search").css("visibility", "hidden");
     });
 
     $("#ex3").click(function(){
-        $("#user_show").css("left", "80em");
-        $("#menu").css("visibility", "visible");
-        $("#logo").css("visibility", "visible");
-        $("#signup_but").css("visibility", "visible");
-        $("#login_but").css("visibility", "visible");
-        $("#search_but").css("visibility", "visible");
-        $("#title").css("visibility", "visible");
-        $("#goer").css("visibility", "visible");
-        $("#search").css("visibility", "visible");
+        $("#user_show").css("left", "83em");
     });
 });
 
