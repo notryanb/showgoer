439021d585ebe4213414e0913c10c95b6a61e1a9
notryanb <notryanb@gmail.com>
2015-04-20 17:37:34 -0400

    Modify for Dummy ENV keys and extra seeds

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,18 @@
-AWS.config(
-  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-)
+if Rails.env.production?
+  AWS.config(
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  )
 
-S3_BUCKET = AWS::S3.new.buckets[ENV['S3_BUCKET']]
+  S3_BUCKET = AWS::S3.new.buckets[ENV['S3_BUCKET']]
+else
+  AWS.config(
+    :access_key_id => 'x',
+    :secret_access_key => 'y'
+  )
+
+  S3_BUCKET = AWS::S3.new.buckets[ENV['S3_BUCKET']]
+
+end
+
 
