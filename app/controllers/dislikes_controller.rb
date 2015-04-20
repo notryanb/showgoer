98862a0be2a774fce558d08334b36e4319df6fc0
notryanb <notryanb@gmail.com>
2015-04-20 12:41:50 -0400

    Ajax for Like/Dislike buttons -> Concert Show

--- app/controllers/dislikes_controller.rb
+++ app/controllers/dislikes_controller.rb
@@ -1,9 +1,18 @@
 class DislikesController < ApplicationController
 
   def create
+    p "HITTING THE DISLIKE POST ROUTE"
     @dislike = Dislike.create(user_id: params[:user_id], video_id: params[:video_id])
 
-    redirect_to :back
+    video = Video.find_by(id: params[:video_id])
+
+    dislike_count = video.dislikes.count
+    p "Dislike Count: #{dislike_count}"
+    if request.xhr?
+      render json:dislike_count, layout: false
+    else
+      redirect_to :back
+    end
   end
 
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,16 @@ class LikesController < ApplicationController
 
 
   def create
-    p "PARAMS #{params}------------------"
-
     @like = Like.create(user_id: params[:user_id], video_id: params[:video_id])
 
-    redirect_to :back
+    video = Video.find_by(id: params[:video_id])
+    like_count = video.likes.count
+    p "Like Count: #{like_count}"
+    if request.xhr?
+      render json:like_count, layout: false
+    else
+      redirect_to :back
+    end
   end
 
 
