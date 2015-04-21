c5502bdc6b12f2fe5850d4282a05eba946415376
notryanb <notryanb@gmail.com>
2015-04-21 18:12:23 -0400

    Added Thumnail upload - new migration for video column

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,18 +1,18 @@
-if Rails.env.production?
+# if Rails.env.production?
 AWS.config(
   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
 )
 
 S3_BUCKET = AWS::S3.new.buckets[ENV['S3_BUCKET']]
-else
-  AWS.config(
-    :access_key_id => 'x',
-    :secret_access_key => 'y'
-  )
+# else
+#   AWS.config(
+#     :access_key_id => 'x',
+#     :secret_access_key => 'y'
+#   )
 
-  S3_BUCKET = AWS::S3.new.buckets[ENV['S3_BUCKET']]
+#   S3_BUCKET = AWS::S3.new.buckets[ENV['S3_BUCKET']]
 
-end
+# end
 
 
