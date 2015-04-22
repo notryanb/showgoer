b0c8fb9ef3010321fa2f61bb64377dcb79924057
Phanendar Chitraju <phanendar@gmail.com>
2015-04-22 11:38:18 -0400

    added the comments feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,19 +5,45 @@ class CommentsController < ApplicationController
   end
 
   def create
-  	@video = Video.find_by(:id => params[:id])
-    new_comment = Comment.new(comment_params)
-    if new_comment.save!
+    @concertid = params[:comment][:theconcert_id]
+    @new_comment = Comment.new(comment_params)
+    if @new_comment.save!
       flash[:notice] = "Thank You for Commenting!"
-      redirect_to "/concerts/#{@video.concert_id}"
+      redirect_to concert_path(@concertid)
     else
       flash.now[:notice] = 'There was an error'
       render :new
     end
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+  comment = Comment.find(params[:id])
+  video = Video.find_by(id: comment.video_id)
+  concert = video.concert_id
+  if comment.update(comment_params)
+   redirect_to concert_path(concert)
+ else
+  [404, "The Page cannot be retrived"]
+end
+end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    video = Video.find_by(id: comment.video_id)
+    concert = video.concert_id
+    comment.destroy
+    respond_to do |format|
+      format.html { redirect_to concert_path(concert), notice: 'The Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
   def comment_params
-    params.require(:comment).permit(:content, :user_id, :video_id)
+    params.require(:comment).permit(:content, :video_id, :user_id)
   end
 end
