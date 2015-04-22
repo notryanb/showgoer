9de5076270d56b37b551474f730de1de12de6d59
Phanendar Chitraju <phanendar@gmail.com>
2015-04-22 17:29:23 -0400

    comments ajax in progress

---
+++ app/assets/javascripts/videocomments.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+$(".post_com").click(function(event){
+    event.preventDefault();
+    $(".the_new_form").toggle();
+    }); //create_button ending
+
+   // AJAX call for posting new comment
+   $(".the_new_form").submit(function(event){
+    event.preventDefault();
+    $target = $(event.target);
+    var form = this;
+    var commentvideoID = $('#comment_video_id').data('videoId');
+    var commentuserID = $('#comment_user_id').data('userId');
+    var commentconcertID = $('#comment_concert_id').data('concertId');
+    console.log(commentvideoID);
+    console.log(commentuserID);
+    console.log(commentconcertID);
+    $.ajax({
+      url: "/comments", // /greeting
+      type: "POST", // 'post'
+      data: { "video_id": commentvideoID, "user_id": commentuserID, "concert_id": commentconcertID },
+      // data: $target.serialize(),
+      dataType: 'json'
+    }).done(function(response) {
+      console.log(response);
+      // $('.comments').append(response);
+      form.reset(); // clear form
+    })
+    .fail(function(err) {
+      console.log("error:", err);
+    })
+    .always(function() {
+      console.log("complete");
+    });
+  });
+
+   }); //ends the documant.ready
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,15 +5,24 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @concertid = params[:comment][:theconcert_id]
-    @new_comment = Comment.new(comment_params)
-    if @new_comment.save!
+    p "***********************************************"
+    p "These are the comment_params: #{comment_params}"
+    if request.xhr?
+      @new_comment = comment.new(content: , video_id: , user_id: )
+      render json: @new_comment.content, layout: false
       flash[:notice] = "Thank You for Commenting!"
-      redirect_to concert_path(@concertid)
     else
-      flash.now[:notice] = 'There was an error'
-      render :new
+      @concertid = params[:comment][:theconcert_id]
+      @new_comment = Comment.new(comment_params)
+
     end
+    # if @new_comment.save!
+    #   flash[:notice] = "Thank You for Commenting!"
+    #   redirect_to concert_path(@concertid)
+    # else
+    #   flash.now[:notice] = 'There was an error'
+    #   render :new
+    # end
   end
 
   def edit
@@ -21,29 +30,29 @@ class CommentsController < ApplicationController
   end
 
   def update
-  comment = Comment.find(params[:id])
-  video = Video.find_by(id: comment.video_id)
-  concert = video.concert_id
-  if comment.update(comment_params)
-   redirect_to concert_path(concert)
- else
-  [404, "The Page cannot be retrived"]
-end
-end
-
-  def destroy
     comment = Comment.find(params[:id])
     video = Video.find_by(id: comment.video_id)
     concert = video.concert_id
-    comment.destroy
-    respond_to do |format|
-      format.html { redirect_to concert_path(concert), notice: 'The Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    if comment.update(comment_params)
+     redirect_to concert_path(concert)
+   else
+    [404, "The Page cannot be retrived"]
   end
+end
 
-  private
-  def comment_params
-    params.require(:comment).permit(:content, :video_id, :user_id)
+def destroy
+  comment = Comment.find(params[:id])
+  video = Video.find_by(id: comment.video_id)
+  concert = video.concert_id
+  comment.destroy
+  respond_to do |format|
+    format.html { redirect_to concert_path(concert), notice: 'The Comment was successfully destroyed.' }
+    format.json { head :no_content }
   end
+end
+
+private
+def comment_params
+  params.require(:comment).permit(:content, :video_id, :user_id)
+end
 end
