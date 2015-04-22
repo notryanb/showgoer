83fcded396c842977016e307a31ceef75d23100f
mbouzi <mrbouzi@gmail.com>
2015-04-22 10:22:31 -0400

    Add videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,20 +8,15 @@ class VideosController < ApplicationController
 
  #CREATION OF VIDEO AND ASSOCATION TO CONCERT / ARTIST
   def create
-
-    # Hash filename for upload so there are no file overwrites.
     thumbnail = "#{SecureRandom.uuid}thumbnail.jpg"
     thumb_url = "https://s3.amazonaws.com/dbc.showgoer.videos/#{thumbnail}"
-
     @video = Video.new(url: params[:url], thumbnail_url: thumb_url, concert_id: params[:concert_id], user_id: params[:user_id])
 
     if @video.save
 
-    # Sub https with http due to FFMPEG SSL issue and encode for URI compatibility.
     http = params[:url].sub(/([https])\w+/,'http')
     http = URI::encode(http)
 
-    # Thumbnails are stored in APP root for now and referenced in FFMPEG uploader.
     base = Rails.root.to_path
 
     system("ffmpeg -i #{http} -ss 00:00:10 -vframes 1 -f image2 #{thumbnail}")
